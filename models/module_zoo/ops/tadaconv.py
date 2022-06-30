3fc88063b81bfd8f966899e1839ba14088967b7f
黄子渊 <tjhuangziyuan@gmail.com>
2022-06-30 10:24:34 +0800

    add implementation for TAdaConv3d

--- models/module_zoo/ops/tadaconv.py
+++ models/module_zoo/ops/tadaconv.py
@@ -154,3 +154,97 @@ class TAdaConv2d(nn.Module):
     def __repr__(self):
         return f"TAdaConv2d({self.in_channels}, {self.out_channels}, kernel_size={self.kernel_size}, " +\
             f"stride={self.stride}, padding={self.padding}, bias={self.bias is not None}, cal_dim=\"{self.cal_dim}\")"
+
+class TAdaConv3d(nn.Module):
+    """
+    Performs temporally adaptive 3D convolution.
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 stride=1, padding=0, dilation=1, groups=1, bias=True,
+                 cal_dim="cin"):
+        super(TAdaConv3d, self).__init__()
+        """
+        Args:
+            in_channels (int): number of input channels.
+            out_channels (int): number of output channels.
+            kernel_size (list): kernel size of TAdaConv2d. 
+            stride (list): stride for the convolution in TAdaConv3d.
+            padding (list): padding for the convolution in TAdaConv3d.
+            dilation (list): dilation of the convolution in TAdaConv3d.
+            groups (int): number of groups for TAdaConv3d. 
+            bias (bool): whether to use bias in TAdaConv3d.
+        """
+
+        kernel_size = _triple(kernel_size)
+        stride = _triple(stride)
+        padding = _triple(padding)
+        dilation = _triple(dilation)
+
+        assert stride[0] == 1
+        assert dilation[0] == 1
+        assert cal_dim in ["cin", "cout"]
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        self.cal_dim = cal_dim
+
+        # base weights (W_b)
+        self.weight = nn.Parameter(
+            torch.Tensor(1, 1, out_channels, in_channels // groups, kernel_size[0], kernel_size[1], kernel_size[2])
+        )
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(1, 1, out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, x, alpha):
+        """
+        Args:
+            x (tensor): feature to perform convolution on.
+            alpha (tensor): calibration weight for the base weights.
+                W_t = alpha_t * W_b
+        """
+        _, _, c_out, c_in, kt, kh, kw = self.weight.size()
+        b, c_in, t, h, w = x.size()
+        x = torch.nn.functional.pad(x, (0,0,0,0,kt//2,kt//2), "constant", 0).unfold(
+            dimension=2, size=kt, step=1
+        ).permute(0,2,1,5,3,4).reshape(1, -1, kt, h, w)
+
+        if self.cal_dim == "cin":
+            # w_alpha: B, C, T, H(1), W(1) -> B, T, C, H(1), W(1) -> B, T, 1, C, H(1), W(1) -> B, T, 1, C, 1, H(1), W(1)
+            # corresponding to calibrating the input channel
+            weight = (alpha.permute(0,2,1,3,4).unsqueeze(2).unsqueeze(-1) * self.weight).reshape(-1, c_in//self.groups, kt, kh, kw)
+        elif self.cal_dim == "cout":
+            # w_alpha: B, C, T, H(1), W(1) -> B, T, C, H(1), W(1) -> B, T, C, 1, H(1), W(1)
+            # corresponding to calibrating the input channel
+            weight = (alpha.permute(0,2,1,3,4).unsqueeze(3).unsqueeze(-1) * self.weight).reshape(-1, c_in//self.groups, kt, kh, kw)
+
+        bias = None
+        if self.bias is not None:
+            # in the official implementation of TAda2D, 
+            # there is no bias term in the convs
+            # hence the performance with bias is not validated
+            bias = self.bias.repeat(b, t, 1).reshape(-1)
+        output = F.conv3d(
+            x, weight=weight, bias=bias, stride=[1] + list(self.stride[1:]), padding=[0] + list(self.padding[1:]),
+            dilation=[1] + list(self.dilation[1:]), groups=self.groups * b * t)
+
+        output = output.view(b, t, c_out, output.size(-2), output.size(-1)).permute(0,2,1,3,4)
+
+        return output
+        
+    def __repr__(self):
+        return f"TAdaConv3d({self.in_channels}, {self.out_channels}, kernel_size={self.kernel_size}, " +\
+            f"stride={self.stride}, padding={self.padding}, bias={self.bias is not None}, cal_dim=\"{self.cal_dim}\")"
